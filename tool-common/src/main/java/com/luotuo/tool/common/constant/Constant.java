beb68b8d1cf7c2bff7076c04c72c62fdfe36c5e5
lujiliang <MasterLu163@163.com>
2018-02-28 23:17:03 +0800

    建立常量类

---
+++ tool-common/src/main/java/com/luotuo/tool/common/constant/Constant.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name:tool-common
+ * File Name:Constant.java
+ * Package Name:com.luotuo.tool.common
+ * Date:2018年2月28日下午11:14:44
+ * Copyright (c) 2018, 鲁济良 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.common.constant;
+/**
+ * ClassName:Constant <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年2月28日 下午11:14:44 <br/>
+ * @author   鲁济良
+ * @since    JDK 1.8	 
+ */
+public class Constant {
+
+}
+
