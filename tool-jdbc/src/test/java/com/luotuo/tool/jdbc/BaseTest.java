6115d2c568ce3b189af54a55757d0e0996102da4
luotuo <masterlu163@163.com>
2018-03-01 14:00:15 +0800

    补全目录

---
+++ tool-jdbc/src/test/java/com/luotuo/tool/jdbc/BaseTest.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:tool-jdbc
+ * File Name:BaseTest.java
+ * Package Name:com.luotuo.tool.jdbc
+ * Date:2018年3月1日下午1:57:00
+ * Copyright (c) 2018, 鲁济良 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.jdbc;
+/**
+ * ClassName:BaseTest <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年3月1日 下午1:57:00 <br/>
+ * @author   鲁济良
+ * @version  1.0
+ * @since    JDK 1.8
+ * @see 	 
+ */
+public class BaseTest {
+
+}
+
