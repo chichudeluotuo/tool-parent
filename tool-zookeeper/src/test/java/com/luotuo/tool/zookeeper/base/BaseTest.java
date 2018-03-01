c4397df4ec0c6cb7c26ebd632897bb6226ea30de
luotuo <masterlu163@163.com>
2018-03-01 13:38:00 +0800

    初始化文件

---
+++ tool-zookeeper/src/test/java/com/luotuo/tool/zookeeper/base/BaseTest.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:tool-zookeeper
+ * File Name:BaseTest.java
+ * Package Name:com.luotuo.tool.zookeeper.base
+ * Date:2018年3月1日下午1:37:26
+ * Copyright (c) 2018, 鲁济良 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.zookeeper.base;
+/**
+ * ClassName:BaseTest <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年3月1日 下午1:37:26 <br/>
+ * @author   鲁济良
+ * @version  1.0
+ * @since    JDK 1.8
+ * @see 	 
+ */
+public class BaseTest {
+
+}
+
