2b43c6934920a5b0b8ede6386f41eaf9205a0301
luotuo <masterlu163@163.com>
2018-03-01 13:31:07 +0800

    zookeeper分支初始化

---
+++ tool-zookeeper/src/main/java/com/luotuo/tool/zookeeper/BootStart.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:tool-zookeeper
+ * File Name:BootStart.java
+ * Package Name:com.luotuo.tool.zookeeper
+ * Date:2018年3月1日下午1:30:25
+ * Copyright (c) 2018, 鲁济良 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.zookeeper;
+/**
+ * ClassName:BootStart <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年3月1日 下午1:30:25 <br/>
+ * @author   鲁济良
+ * @version  1.0
+ * @since    JDK 1.8
+ * @see 	 
+ */
+public class BootStart {
+
+}
+
