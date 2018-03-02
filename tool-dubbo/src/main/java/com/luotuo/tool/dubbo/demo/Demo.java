4a0682e2b1bcbd97fb45d9a4994d53322de25b58
lujiliang <MasterLu163@163.com>
2018-03-02 23:30:58 +0800

    初始化tool-dubbo

---
+++ tool-dubbo/src/main/java/com/luotuo/tool/dubbo/demo/Demo.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name:tool-dubbo
+ * File Name:Demo.java
+ * Package Name:com.luotuo.tool.dubbo.demo
+ * Date:2018年3月2日下午11:29:45
+ * Copyright (c) 2018, 鲁济良 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.dubbo.demo;
+/**
+ * ClassName:Demo <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年3月2日 下午11:29:45 <br/>
+ * @author   鲁济良
+ * @since    JDK 1.8	 
+ */
+public class Demo {
+
+}
+
---
+++ tool-dubbo/src/test/java/com/luotuo/tool/dubbo/bootstrap/BootStrap.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:tool-dubbo
+ * File Name:BootStrap.java
+ * Package Name:com.luotuo.tool.dubbo.bootstrap
+ * Date:2018年3月2日下午10:56:44
+ * Copyright (c) 2018, 鲁济良 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.dubbo.bootstrap;
+
+/**
+ * ClassName:BootStrap <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2018年3月2日 下午10:56:44 <br/>
+ * 
+ * @author 鲁济良
+ * @since JDK 1.8
+ */
+public class BootStrap {
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
