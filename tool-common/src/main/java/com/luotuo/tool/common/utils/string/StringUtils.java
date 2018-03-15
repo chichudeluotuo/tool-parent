6f7946d1309254239fa9170f5659c75127e9f8ae
lujiliang <MasterLu163@163.com>
2018-03-15 23:03:39 +0800

    更新类

--- tool-common/src/main/java/com/luotuo/tool/common/utils/string/StringUtils.java
+++ tool-common/src/main/java/com/luotuo/tool/common/utils/string/StringUtils.java
@@ -9,26 +9,17 @@
 
 package com.luotuo.tool.common.utils.string;
 
-import org.springframework.core.convert.converter.Converter;
-
 /**
  * ClassName:StringUtils <br/>
  * Function: TODO ADD FUNCTION. <br/>
- * Reason:	 TODO ADD REASON. <br/>
- * Date:     2018年3月2日 下午1:42:34 <br/>
- * @author   鲁济良
- * @version  1.0
- * @since    JDK 1.8
- * @see 	 
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2018年3月2日 下午1:42:34 <br/>
+ * 
+ * @author 鲁济良
+ * @version 1.0
+ * @since JDK 1.8
+ * @see
  */
-public final class StringUtils implements  Converter<String,Enum>{
-
-    public Enum convert(String arg0) {
-        
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+public final class StringUtils {
 
 }
-
