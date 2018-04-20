417fa35cb07399fc44feb6555333be32764ec2ef
master <masterlu163@163.com>
2018-04-20 10:00:03 +0800

    栈的装饰实现

---
+++ tool-common/src/main/java/com/luotuo/tool/common/utils/arithmetic/LuStack.java
@@ -0,0 +1,38 @@
+/**
+ * Project Name:tool-common
+ * File Name:StackSeries.java
+ * Package Name:com.luotuo.tool.common.utils.arithmetic
+ * Date:2018年4月20日上午9:24:14
+ * Copyright (c) 2018, 彳亍的骆驼 All Rights Reserved.
+ *
+*/
+
+package com.luotuo.tool.common.utils.arithmetic;
+
+import java.util.Stack;
+
+/**
+ * ClassName: Stack <br/>
+ * Function:1.实现一个栈，带有出栈(pop),入栈(push),取最小元素(getMin)三个方法，保证三个方法的时间复杂度为O(1). <br/>
+ * Reason: TODO ADD REASON(可选). <br/>
+ * date: 2018年4月20日 上午9:45:08 <br/>
+ *
+ * @author 鲁济良
+ * @since JDK 1.8
+ */
+public class LuStack<E> extends Stack<E>{
+	
+	/**
+	 * serialVersionUID:TODO(描述这个变量表示什么).
+	 * @since JDK 1.8
+	 */
+	private static final long serialVersionUID = -6086986387594872075L;
+
+	public E getMin() {
+
+	
+		return null;
+	}
+	
+
+}
